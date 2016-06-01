7bbb35ef624e50ddfe465ee2e979add065ace6d8
deveshjadon <devesh.jadon@venturepact.com>
2016-06-01 19:51:59 +0530

    sailsdemo project start

---
+++ api/controllers/SiteControllerController.js
@@ -0,0 +1,51 @@
+/**
+ * SiteControllerController
+ *
+ * @description :: Server-side logic for managing Sitecontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `SiteControllerController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SiteControllerController.destroy()`
+   */
+  destroy: function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SiteControllerController.tag()`
+   */
+  tag: function (req, res) {
+    return res.json({
+      todo: 'tag() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SiteControllerController.like()`
+   */
+  like: function (req, res) {
+    return res.json({
+      todo: 'like() is not implemented yet!'
+    });
+  }
+};
+
